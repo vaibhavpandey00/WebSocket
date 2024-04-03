1369a1a2fcf1356e5e6a85b54170fb22444a38e5
vaibhav Pandey <vaibhav.pandey0798@gmail.com>
2024-04-03 21:12:44 +0530

    SocketIO

---
+++ Server/index.js
@@ -0,0 +1,25 @@
+const http = require("http")
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io
+io.on("connection", (socket) => {
+    // console.log("A new user has connected", socket.id);
+    socket.on("user-message", (message) => {
+        // console.log("A new user Message", message);
+        io.emit("message", message);
+    })
+})
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+    return res.sendFile("/public/index.html")
+})
+
+server.listen(9000, () => console.log("listening.."))
